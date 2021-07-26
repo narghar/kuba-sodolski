2c3ce701d8a113b10a09ebaf166600640a26a0c5
Igor Wysocki <narghar@gmail.com>
2021-07-26 14:36:05 +0200

    Update lorem with text

--- src/views/faq/mainFaq.js
+++ src/views/faq/mainFaq.js
@@ -17,39 +17,51 @@ const MainFAQ = () => {
 
 const questionList = [
   {
-    question: "Ile kosztuje tłumaczenie?",
+    question: "Co to jest tłumaczenie symultaniczne?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis magna eros. Aliquam erat volutpat. Fusce volutpat nisi non pulvinar ornare. Donec efficitur mauris non tortor consectetur, non lacinia diam imperdiet.",
+      "Symultaniczne = równoczesne. Tłumacz jednocześnie słucha wypowiedzi, w czasie rzeczywistym przekłada w głowie na drugi język i mówi w tym języku. Niesamowite, prawda? Do tłumaczenia symultanicznego zwykle stosuje się wygłuszające kabiny tłumaczeniowe i specjalną aparaturę konferencyjną. Na kameralnych spotkaniach można tłumaczyć symultanicznie do ucha odbiorcy, siedząc obok niego. Taki rodzaj tłumaczenia symultanicznego to tłumaczenie szeptane.",
     active: true,
   },
   {
-    question: "Dlaczego tłumacz rozlicza się w dniach?",
+    question: "Co to jest tłumaczenie konsekutywne?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis magna eros. Aliquam erat volutpat. Fusce volutpat nisi non pulvinar ornare. Donec efficitur mauris non tortor consectetur, non lacinia diam imperdiet.",
+      "Prelegent mówi, a tłumacz robi w tym czasie notatki w specjalnym systemie notacji. Kiedy mówca skończy – tłumacz ustny odtwarza wystąpienie w drugim języku. Najlepsi tłumacze potrafią w ten sposób precyzyjnie przełożyć nawet bardzo długie wypowiedzi, trwające 6 minut i więcej!",
     active: false,
   },
   {
-    question: "Dlaczego tłumacz prosi o przesałnie prezentacji? ",
+    question: "Czy mogę zamówić tłumaczenie zdalne?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis magna eros. Aliquam erat volutpat. Fusce volutpat nisi non pulvinar ornare. Donec efficitur mauris non tortor consectetur, non lacinia diam imperdiet.",
+      "Tak, praktycznie każdy typ tłumaczenia możemy wykonać zdalnie! Wachlarz możliwości jest bardzo szeroki: od dedykowanych narzędzi, przez proste dodatki do popularnych komunikatorów, aż po najprostsze rozwiązania oparte na zwykłym telefonie. Dzięki tym rozwiązaniom możesz cieszyć się płynną komunikacją i profesjonalnym tłumaczeniem również na spotkaniu online.",
     active: false,
   },
   {
-    question: "Jaki rodzaj tłumaczenie bedzie dla mnie najlepszy?",
+    question: "Jakiego rodzaju tłumaczenia potrzebuję?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis magna eros. Aliquam erat volutpat. Fusce volutpat nisi non pulvinar ornare. Donec efficitur mauris non tortor consectetur, non lacinia diam imperdiet.",
+      "Oprócz tłumaczeń symultanicznych i konsekutywnych, istnieje cała gama ich odmian. Doświadczony tłumacz doradzi Ci rozwiązanie, które najlepiej sprawdzi się na wydarzeniu, które planujesz. Dlatego zadzwoń, aby rozwiać wszelkie wątpliwości!",
     active: false,
   },
   {
     question: "Czy muszę osobno zamówić sprzęt?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis magna eros. Aliquam erat volutpat. Fusce volutpat nisi non pulvinar ornare. Donec efficitur mauris non tortor consectetur, non lacinia diam imperdiet.",
+      "Zależy, jaki wariant tłumaczenia będzie realizowany. Przy tłumaczeniu symultanicznym w większości przypadków potrzebny jest sprzęt. Możesz go zamówić u mnie razem z tłumaczeniem – zwykle uzyskasz tym sposobem najlepszy stosunek jakości sprzętu do ceny jego wynajęcia.",
     active: false,
   },
   {
-    question: "Dlaczego tłumacze pracują w zespole dwuosobowym",
+    question: "Dlaczego tłumacze proszą o wysłanie prezentacji?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis magna eros. Aliquam erat volutpat. Fusce volutpat nisi non pulvinar ornare. Donec efficitur mauris non tortor consectetur, non lacinia diam imperdiet.",
+      "Dopytywanie o prezentacje i materiały bardzo dobrze świadczy o tłumaczu! Pamiętaj, że jako tłumacze jesteśmy na Twoim wydarzeniu ludźmi spoza Twojej organizacji. Nawet kiedy mamy szerokie doświadczenie w danej branży, potrzebujemy przygotować się do konkretnego spotkania. Prosimy o wszelkie dostępne materiały, ponieważ chcemy dać Ci najlepsze możliwe tłumaczenie!",
+    active: false,
+  },
+  {
+    question: "Dlaczego potrzeba dwojga tłumaczy?",
+    answer:
+      "Jednocześnie słuchamy, mówimy (albo notujemy i zapamiętujemy) oraz przetwarzamy wypowiedź klienta w głowie. To proces, który wymaga ekstremalnej koncentracji i nie sposób tłumaczyć bez przerwy. Dlatego tłumacze zmieniają się co 15-30 minut. W przerwie od tłumaczenia pracujemy nad terminologią i wspieramy tłumaczącego kolegę.",
+    active: false,
+  },
+  {
+    question: "Dlaczego tłumacze rozliczają się w dniówkach?",
+    answer:
+      "Zwykle tłumacz nie może przyjąć więcej niż jedno zlecenie danego dnia. Ponadto przygotowanie merytoryczne i leksykalne do tłumaczenia często trwa dłużej niż samo tłumaczenie. W cenie tłumaczenia zawarty jest nie tylko czas samego spotkania, ale też czas niezbędnych przygotowań.",
     active: false,
   },
 ];
--- src/views/services/servicesOffer.js
+++ src/views/services/servicesOffer.js
@@ -21,33 +21,62 @@ const ServicesOffer= () => {
 const services = [
   {
     item: "01",
-    title: [<>Tłumaczenie<br/>zdalne</>],
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus enim nunc, vestibulum nec malesuada in, hendrerit a felis. ",
+    title: [
+      <>
+        Tłumaczenie
+        <br />
+        zdalne
+      </>,
+    ],
+    text: "Pełne bezpieczeństwo epidemiologiczne i optymalizacja kosztów dzięki tłumaczeniu zdalnemu, wybierz z gamy rozwiązań dla tłumaczeń online, skuteczne tłumaczenie spotkań na Zoom, MS Teams i innych platformach, możliwość zastosowania dla tłumaczenia symultanicznego i konsekutywego",
   },
   {
     item: "02",
-    title: [<>Tłumaczenie<br/>symultaniczne</>],
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus enim nunc, vestibulum nec malesuada in, hendrerit a felis. ",
+    title: [
+      <>
+        Tłumaczenie
+        <br />
+        symultaniczne
+      </>,
+    ],
+    text: "Tłumaczenie w czasie rzeczywistym, największa kompletność i precyzja, możliwość prowadzenia dyskusji bez przerw na tłumaczenie, oszczędność czasu, tłumacz jest niewidoczny",
   },
   {
     item: "03",
-    title: [<>Tłumaczenie<br/>konsekutywne</>],
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus enim nunc, vestibulum nec malesuada in, hendrerit a felis. ",
+    title: [
+      <>
+        Tłumaczenie
+        <br />
+        konsekutywne
+      </>,
+    ],
+    text: "Idealne do krótkich tłumaczeń na uroczystych wydarzeniach i konferencjach prasowych, oszczędność – nie jest konieczny sprzęt konferencyjny, wybierz tłumacza, który potrafi dopasować się do charakteru Twojego wydarzenia, bo będzie widoczny obok prelegenta",
   },
   {
     item: "04",
-    title: [<>Tłumaczenie<br/>towarzyszące</>],
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus enim nunc, vestibulum nec malesuada in, hendrerit a felis. ",
+    title: [
+      <>
+        Tłumaczenie
+        <br />
+        towarzyszące
+      </>,
+    ],
+    text: "Negocjacje? audyt? wizyta studyjna? lunch biznesowy? W takich przypadkach najlepiej sprawdzi się tłumaczenie towarzyszące, stanowiące hybrydę konsekutywnego i symultanicznego – tłumacz jest w każdej chwili u boku klienta i zapewnia dynamicznie płynną komunikację niezależnie od sytuacji",
   },
   {
     item: "05",
-    title: [<>Sprzęt tłumaczeniowy<br/> i nagłośnieniowy</>],
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus enim nunc, vestibulum nec malesuada in, hendrerit a felis. ",
+    title: [
+      <>
+        Sprzęt tłumaczeniowy
+        <br /> i nagłośnieniowy
+      </>,
+    ],
+    text: "Jeśli zamawiasz tłumaczenie wymagające sprzętu konferencyjnego, możesz go zamówić u mnie, by zapewnić sobie wysoką jakość aparatury i uniknąć kosztu pośrednika",
   },
   {
     item: "06",
     title: [<>Wiele języków</>],
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus enim nunc, vestibulum nec malesuada in, hendrerit a felis. ",
+    text: "Osobiście tłumaczę angielski, włoski i francuski, a oprócz tego współpracuję ze sprawdzonym gronem profesjonalnych tłumaczy wielu języków, dzięki czemu możesz zlecić obsługę językową dużej konferencji jednemu podmiotowi",
   },
 ];
 
